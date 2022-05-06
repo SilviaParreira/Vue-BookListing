0764e5fdc9f42647a3780ed6ff5396be53f35f98
SilviaParreira <33030228+SilviaParreira@users.noreply.github.com>
2022-05-06 17:12:58 +0100

    module2

---
+++ src/components/BookItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+      <li v-for="book in books" :key="book.title">
+                {{book.title}}:{{book.author}}
+            </li>
+  </ul>
+</template>
+
+<script>
+    export default {
+        name: 'BookItem',
+        props: ['book']
+    }
+</script>
+
+<style>
+
+</style>
